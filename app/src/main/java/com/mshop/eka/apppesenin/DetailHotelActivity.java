42169a013313f428035af8064c83e03cc5310955
NantaArthur <45324679+NantaArthur@users.noreply.github.com>
2018-11-26 19:59:33 +0700

    Nanta Ganteng

--- app/src/main/java/com/mshop/eka/apppesenin/DetailHotelActivity.java
+++ app/src/main/java/com/mshop/eka/apppesenin/DetailHotelActivity.java
@@ -5,17 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class DetailHotelActivity extends AppCompatActivity {
     Button b_hotel_pilih_kamar;
-
+    TextView tv_hotel_nama;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_hotel);
+        tv_hotel_nama = (TextView) findViewById(R.id.tv_hotel_nama);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        RecyclerItemHotel itemHotel = (RecyclerItemHotel) getIntent().getSerializableExtra("hotel");
+        tv_hotel_nama.setText(itemHotel.getJudul());
         b_hotel_pilih_kamar = (Button) findViewById(R.id.b_hotel_pilih_kamar);
 
         b_hotel_pilih_kamar.setOnClickListener(new View.OnClickListener() {
