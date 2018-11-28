1a324cdb4e6e36836a37cfd2b7a39f4eea2e6a95
NantaArthur <45324679+NantaArthur@users.noreply.github.com>
2018-11-28 12:37:17 +0700

    Nanta Ganteng season 2

---
+++ app/src/main/java/com/mshop/eka/apppesenin/Kamar.java
@@ -0,0 +1,39 @@
+package com.mshop.eka.apppesenin;
+
+public class Kamar {
+    private int id;
+    private String judul,makstamu,fasilitas1,fasilitas2,harga;
+
+    public Kamar(int id, String judul, String makstamu, String fasilitas1, String fasilitas2, String harga) {
+        this.id = id;
+        this.judul = judul;
+        this.makstamu = makstamu;
+        this.fasilitas1 = fasilitas1;
+        this.fasilitas2 = fasilitas2;
+        this.harga = harga;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public String getMakstamu() {
+        return makstamu;
+    }
+
+    public String getFasilitas1() {
+        return fasilitas1;
+    }
+
+    public String getFasilitas2() {
+        return fasilitas2;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+}
--- app/src/main/java/com/mshop/eka/apppesenin/KamarHotelActivity.java
+++ app/src/main/java/com/mshop/eka/apppesenin/KamarHotelActivity.java
@@ -5,15 +5,73 @@ import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KamarHotelActivity extends AppCompatActivity {
     ConstraintLayout c_layout_hotel_kamar;
+    List<Kamar> kamarList;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kamar_hotel);
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv_layout_kamar);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        kamarList = new ArrayList<>();
+
+        kamarList.add(new Kamar(
+                1,
+                "ekaHomo",
+                "Maks 69 tamu",
+                "PS4",
+                "Makan Pagi",
+                "Rp. 399.000.000"
+
+        ));
+        kamarList.add(new Kamar(
+                2,
+                "GayVillage",
+                "Maks 69 tamu",
+                "PS4",
+                "Makan Pagi",
+                "Rp. 399.000.000"
+
+        ));
+        kamarList.add(new Kamar(
+                3,
+                "HUMUEKA",
+                "Maks 69 tamu",
+                "PS4",
+                "Makan Pagi",
+                "Rp. 399.000.000"
+
+        ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
